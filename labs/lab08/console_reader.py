a65f309826fc61eaa8b31f2363dfe0300caadb5c
Vasilii Kamenets <resecmo@gmail.com>
2020-12-19 21:53:10 +0300

    added init, added eoferror handling

--- labs/lab08/console_reader.py
+++ labs/lab08/console_reader.py
@@ -1,7 +1,11 @@
 class ConsoleIterator:
-    def __iter__(self):
+    def __init__(self):
         self._numbers = []
+
+        # flag to denote that no error was encountered; if false, __next__() still works until self.numbers is empty
         self.ok = True
+
+    def __iter__(self):
         return self
 
     def __next__(self):
@@ -9,7 +13,11 @@ class ConsoleIterator:
             if len(self._numbers) > 0:
                 return self._numbers.pop(0)
             elif self.ok:
-                input_string = input().split()
+                try:
+                    input_string = input().split()
+                except EOFError:
+                    raise StopIteration
+
                 try:
                     for item in input_string:
                         self._numbers.append(int(item))
@@ -19,10 +27,11 @@ class ConsoleIterator:
                 raise StopIteration
 
 
-total_sum = 0
+if __name__ == "__main__":
+    total_sum = 0
 
-for number in ConsoleIterator():
-    print(number)
-    total_sum = total_sum + number
+    for number in ConsoleIterator():
+        print(number)
+        total_sum = total_sum + number
 
-print(f'Sum of entered numbers is {total_sum}')
+    print(f'Sum of entered numbers is {total_sum}')
